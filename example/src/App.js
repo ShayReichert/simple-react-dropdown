b17937338adcf57b7b59f7caf2280442c9d563d3
Shay Reichert <shay.reichert@outlook.fr>
2022-10-21 16:14:49 +0200

    Simple select-option

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-
-import { ExampleComponent } from 'simple-react-dropdown'
+import React, { useState } from 'react'
+import { Dropdown } from 'simple-react-dropdown'
 import 'simple-react-dropdown/dist/index.css'
 
+const countries = [
+  { value: 'al', label: 'Alabama' },
+  { value: 'ak', label: 'Alaska' },
+  { value: 'cl', label: 'California' }
+]
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [selectState, setSelectState] = useState('')
+  console.log(selectState)
+  return (
+    <div className='App'>
+      <Dropdown
+        name='state-select'
+        label='State'
+        value={selectState}
+        onChange={setSelectState}
+        options={countries}
+        placeholder='Select a state'
+      />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Dropdown = ({
+  name,
+  label,
+  value,
+  onChange,
+  options,
+  placeholder
+}) => {
+  return (
+    <label className={styles.dropdown}>
+      {label}:
+      <select
+        name={name}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        required
+      >
+        <option key='' value={value} disabled>
+          {placeholder}
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </label>
+  )
 }
