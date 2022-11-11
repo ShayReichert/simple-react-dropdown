ab8c26b3f3d193298af479fd7fb96a73d3e75e58
Shay Reichert <shay.reichert@outlook.fr>
2022-11-11 10:07:06 +0100

    Updates

---
+++ precache-manifest.584f0491435c188187803a274e524d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ef0341f8b5180c11e245b8b6722e51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "938121f373ab011c0206",
+    "url": "./static/css/main.3a7f56de.chunk.css"
+  },
+  {
+    "revision": "bc3574f52aaf16b5dcef",
+    "url": "./static/js/2.89670e51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.89670e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938121f373ab011c0206",
+    "url": "./static/js/main.fe22455f.chunk.js"
+  },
+  {
+    "revision": "3b72900b39bc62556c52",
+    "url": "./static/js/runtime-main.4446b358.js"
+  }
+]);
